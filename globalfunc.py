9c71d1f96115e03b68bbe8b9acabb5dc03d342e6
Amanda <deltaquadwiki@gmail.com>
2020-02-18 04:33:55 -0500

    stupid format causing bug

--- globalfunc.py
+++ globalfunc.py
@@ -107,7 +107,7 @@ def getLastEdit(title):
     history=getHistory(title)
     last = history[0]
     timestamp = last["timestamp"]
-    time = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ').strftime('%b %d %Hh%m UTC')
+    time = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ').strftime('%b %d %Hh%M UTC')
     time = time.replace(" ".join(time.split(" ")[0:2]), "{{Date table sorting|"+" ".join(time.split(" ")[0:2])+"}}")
     return [last["user"],time]
 def getLastClerk(title):
